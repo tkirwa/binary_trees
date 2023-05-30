d2d70014ca23e56a223979f4399d3ca9e82a4715
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-05-30 20:26:33 +0300

    102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,135 +1,118 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
- * link_t - A linked list node to store binary tree nodes
- * @node: Pointer to the binary tree node
- * @next: Pointer to the next linked list node
+ * new_node - Function that creates a new_node in a linked_list
+ * @node: Type pointer of node to be created
+ * Return: the node created
  */
-typedef struct link_s
+link_t *new_node(binary_tree_t *node)
 {
-	binary_tree_t *node;
-	struct link_s *next;
-} link_t;
+link_t *new;
 
-/**
- * new_link - Creates a new linked list node
- * @node: Pointer to the binary tree node
- *
- * Return: The created linked list node
- */
-link_t *new_link(binary_tree_t *node)
+new = malloc(sizeof(link_t));
+if (new == NULL)
 {
-	link_t *new_node = malloc(sizeof(link_t));
-	if (new_node == NULL)
-		return (NULL);
+return (NULL);
+}
+new->node = node;
+new->next = NULL;
 
-	new_node->node = node;
-	new_node->next = NULL;
-
-	return (new_node);
+return (new);
 }
 
 /**
- * free_q - Frees the linked list nodes
- * @head: The head of the linked list
+ * free_q - Function that free the nodes in the linked list
+ * @head: Head node of the linked list
  */
 void free_q(link_t *head)
 {
-	link_t *temp;
+link_t *temp_node;
 
-	while (head)
-	{
-		temp = head;
-		head = head->next;
-		free(temp);
-	}
-}
-
-/**
- * push - Pushes a binary tree node to the queue (linked list)
- * @node: The binary tree node to push
- * @head: The head of the linked list
- * @tail: The tail of the linked list
- *
- * Return: The new tail of the linked list
- */
-link_t *push(binary_tree_t *node, link_t *head, link_t **tail)
+while (head)
 {
-	link_t *new_link_node = new_link(node);
-	if (new_link_node == NULL)
-	{
-		free_q(head);
-		exit(1);
-	}
-
-	(*tail)->next = new_link_node;
-	*tail = new_link_node;
-	return (new_link_node);
+temp_node = head->next;
+free(head);
+head = temp_node;
+}
 }
 
 /**
- * pop - Pops a binary tree node from the queue (linked list)
- * @head: The head of the linked list
+ * _push - Function that pushes a node into the stack
+ * @node: Node of the tree
+ * @head: Head node in the stack
+ * @tail: Tail node in the stack
  */
-void pop(link_t **head)
+void _push(binary_tree_t *node, link_t *head, link_t **tail)
 {
-	link_t *temp;
+link_t *new;
 
-	if (*head == NULL)
-		return;
-
-	temp = *head;
-	*head = (*head)->next;
-	free(temp);
+new = new_node(node);
+if (new == NULL)
+{
+free_q(head);
+exit(1);
+}
+(*tail)->next = new;
+*tail = new;
 }
 
 /**
- * binary_tree_is_complete - Checks if a binary tree is complete
- * @tree: Pointer to the root node of the tree
- *
- * Return: 1 if the tree is complete, 0 otherwise
+ * _pop - Function that pops a node from the stack
+ * @head: Head node in the stack
+ */
+void _pop(link_t **head)
+{
+link_t *temp_node;
+
+temp_node = (*head)->next;
+free(*head);
+*head = temp_node;
+}
+
+/**
+ * binary_tree_is_complete - Function that checks if a binary tree is complete
+ * @tree: Root node of the tree
+ * Return: 1 if it is complete, 0 if it is not
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	link_t *head, *tail;
-	int flag = 0;
+link_t *head, *tail;
+int flag = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	head = tail = new_link((binary_tree_t *)tree);
-	if (head == NULL)
-		exit(1);
-
-	while (head != NULL)
-	{
-		if (head->node->left != NULL)
-		{
-			if (flag == 1)
-			{
-				free_q(head);
-				return (0);
-			}
-			tail = push(head->node->left, head, &tail);
-		}
-		else
-			flag = 1;
-
-		if (head->node->right != NULL)
-		{
-			if (flag == 1)
-			{
-				free_q(head);
-				return (0);
-			}
-			tail = push(head->node->right, head, &tail);
-		}
-		else
-			flag = 1;
-
-		pop(&head);
-	}
-
-	return (1);
+if (tree == NULL)
+{
+return (0);
+}
+head = tail = new_node((binary_tree_t *)tree);
+if (head == NULL)
+{
+exit(1);
+}
+while (head != NULL)
+{
+if (head->node->left != NULL)
+{
+if (flag == 1)
+{
+free_q(head);
+return (0);
+}
+_push(head->node->left, head, &tail);
+}
+else
+flag = 1;
+if (head->node->right != NULL)
+{
+if (flag == 1)
+{
+free_q(head);
+return (0);
+}
+_push(head->node->right, head, &tail);
+}
+else
+flag = 1;
+_pop(&head);
+}
+return (1);
 }
