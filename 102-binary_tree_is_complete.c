024a9981a6bb8209b2c6f84588bfbca0ae383ae1
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-05-30 20:02:57 +0300

    102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -8,15 +8,15 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+if (tree->left == NULL && tree->right == NULL)
+return (1);
 
-    if (tree->left && tree->right)
-        return (binary_tree_is_complete(tree->left) &&
-                binary_tree_is_complete(tree->right));
+if (tree->left && tree->right)
+return (binary_tree_is_complete(tree->left) &&
+binary_tree_is_complete(tree->right));
 
-    return (0);
+return (0);
 }
