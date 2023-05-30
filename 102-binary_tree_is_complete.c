1ebf6a8e4743bddf1fd8475aee5aea52cee4a546
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-05-30 20:30:09 +0300

    102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
  * new_node - Function that creates a new_node in a linked_list
--- binary_trees.h
+++ binary_trees.h
@@ -19,6 +19,14 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+
+typedef struct link_s
+{
+	size_t n;
+	struct binary_tree_s const *node;
+	struct link_s *next;
+} link_t;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
