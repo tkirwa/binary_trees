4852e15151b84c9cf7693b79990aa15541a12121
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-05-30 17:56:49 +0300

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height, right_height;
-
 if (tree == NULL)
 return (0);
 
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
+size_t left_height = binary_tree_height(tree->left);
+size_t right_height = binary_tree_height(tree->right);
 
-if (left_height > right_height)
-return (left_height + 1);
-else
-return (right_height + 1);
+return (1 + (left_height > right_height ? left_height : right_height));
 }
