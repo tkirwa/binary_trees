c3d9f279c573e833121ca93ef3858553f9e8e8ed
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-06-01 11:42:07 +0300

    114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -24,25 +24,33 @@ return (node);
 bst_t *bst_remove(bst_t *root, int value)
 {
 if (root == NULL)
-return NULL;
+return (NULL);
 
 if (value < root->n)
 root->left = bst_remove(root->left, value);
 else if (value > root->n)
 root->right = bst_remove(root->right, value);
-else {
-if (root->left == NULL && root->right == NULL) {
+else
+{
+if (root->left == NULL && root->right == NULL)
+{
 free(root);
 return (NULL);
-} else if (root->left == NULL) {
+}
+else if (root->left == NULL)
+{
 bst_t *temp = root->right;
 free(root);
 return (temp);
-} else if (root->right == NULL) {
+}
+else if (root->right == NULL)
+{
 bst_t *temp = root->left;
 free(root);
 return (temp);
-} else {
+}
+else
+{
 bst_t *temp = bst_find_min(root->right);
 root->n = temp->n;
 root->right = bst_remove(root->right, temp->n);
