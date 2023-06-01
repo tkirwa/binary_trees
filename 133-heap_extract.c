db96858a5e893d080df117b382a5eb97715fa427
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-06-01 15:14:28 +0300

    133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
  * tree_size - Computes the size of a binary tree
  * @tree: Pointer to the root node of the tree
@@ -11,19 +13,49 @@ if (tree == NULL)
 return (0);
 return (1 + tree_size(tree->left) + tree_size(tree->right));
 }
+
 /**
- * heapify_down - Restores the heap property by moving down the tree
- * @node: Pointer to the current node
+ * get_last_node - Gets the last node in level-order traversal
+ * @root: Pointer to the root node of the heap
+ * Return: Pointer to the last node
  */
+heap_t *get_last_node(heap_t *root)
+{
+heap_t *queue[1000];
+heap_t *node;
+size_t rear = 0, front = 0;
+
+if (root == NULL)
+return (NULL);
+
+queue[rear++] = root;
+while (front < rear)
+{
+node = queue[front++];
+if (node->left)
+queue[rear++] = node->left;
+if (node->right)
+queue[rear++] = node->right;
+}
+
+return (node);
+}
+
+/**
+* heapify_down - Restores the heap property by moving down the tree
+* @node: Pointer to the current node
+*/
 void heapify_down(heap_t *node)
 {
 heap_t *largest = node;
 heap_t *left = node->left;
 heap_t *right = node->right;
+
 if (left != NULL && left->n > largest->n)
 largest = left;
 if (right != NULL && right->n > largest->n)
 largest = right;
+
 if (largest != node)
 {
 int temp = node->n;
@@ -32,51 +64,39 @@ largest->n = temp;
 heapify_down(largest);
 }
 }
+
 /**
- * heap_extract - Extracts the root node of a Max Binary Heap
- * @root: Double pointer to the root node of the heap
- * Return: Value stored in the root node, or 0 on failure
- */
+* heap_extract - Extracts the root node of a Max Binary Heap
+* @root: Double pointer to the root node of the heap
+* Return: Value stored in the root node, or 0 on failure
+*/
 int heap_extract(heap_t **root)
 {
 if (root == NULL || *root == NULL)
 return (0);
+
 int extracted_value = (*root)->n;
+
 size_t size = tree_size(*root);
-heap_t *last_node = NULL;
-heap_t *temp = *root;
+heap_t *last_node = get_last_node(*root);
+
 if (size == 1)
+{
+free(*root);
 *root = NULL;
-free(temp);
 return (extracted_value);
-while (temp->left != NULL || temp->right != NULL)
-{
-if (temp->right == NULL)
-last_node = temp->left;
-break;
-if (temp->left->n >= temp->right->n)
-temp = temp->left;
-else
-temp = temp->right;
 }
-if (last_node == NULL)
-{
-last_node = temp;
-temp = temp->parent;
-if (temp->left == last_node)
-temp->left = NULL;
-else
-temp->right = NULL;
-}
-else
-{
-if (temp->left == last_node)
-temp->left = NULL;
-else
-temp->right = NULL;
-}
+
 (*root)->n = last_node->n;
+
+if (last_node->parent->left == last_node)
+last_node->parent->left = NULL;
+else
+last_node->parent->right = NULL;
+
 free(last_node);
+
 heapify_down(*root);
+
 return (extracted_value);
 }
