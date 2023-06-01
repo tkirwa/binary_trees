c3632bc8bd32c9f6d5a572ef93d551fc6ac39698
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-06-01 12:41:59 +0300

    121-avl_insert.c, 14-binary_tree_balance.c, 103-binary_tree_rotate_left.c, 104-binary_tree_rotate_right.c, 0-binary_tree_node.c

--- binary_trees.h
+++ binary_trees.h
@@ -19,7 +19,6 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
