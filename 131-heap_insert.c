bc803ad259199772cc899fb0590e280df9568965
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-06-02 17:52:14 +0300

    fix - 131-heap_insert.c

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -22,7 +22,6 @@ return (left + 1);
 
 return (right + 1);
 }
-
 /**
 * binary_tree_is_perfect - checks if a binary tree is perfect
 *
@@ -35,19 +34,15 @@ if (tree && height(tree->left) == height(tree->right))
 {
 if (height(tree->left) == -1)
 return (1);
-
-if ((tree->left && !(tree->left->left) && !(tree->left->right))
-&& (tree->right && !(tree->right->left) && !(tree->right->right)))
+if ((tree->left && !(tree->left->left) &&!(tree->left->right))
+&&(tree->right && !(tree->right->left) &&!(tree->right->right)))
 return (1);
-
 if (tree && tree->left && tree->right)
 return (binary_tree_is_perfect(tree->left) &&
 binary_tree_is_perfect(tree->right));
 }
-
 return (0);
 }
-
 /**
 * swap - swaps nodes when child is greater than parent
 *
@@ -59,24 +54,19 @@ void swap(heap_t **arg_node, heap_t **arg_child)
 {
 heap_t *node, *child, *node_child, *node_left, *node_right, *parent;
 int left_right;
-
 node = *arg_node;
 child = *arg_child;
-
 if (child->n > node->n)
 {
 if (child->left)
 child->left->parent = node;
 if (child->right)
 child->right->parent = node;
-
 if (node->left == child)
 node_child = node->right, left_right = 0;
 else
 node_child = node->left, left_right = 1;
-
 node_left = child->left, node_right = child->right;
-
 if (left_right == 0)
 {
 child->right = node_child;
@@ -91,7 +81,6 @@ if (node_child)
 node_child->parent = child;
 child->right = node;
 }
-
 if (node->parent)
 {
 if (node->parent->left == node)
@@ -99,7 +88,6 @@ node->parent->left = child;
 else
 node->parent->right = child;
 }
-
 parent = node->parent, child->parent = parent;
 node->parent = child, node->left = node_left;
 node->right = node_right, *arg_node = child;
